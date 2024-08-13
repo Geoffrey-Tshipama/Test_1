7c81dfb207b4c9be09d8db991649cf60b4d43bdf
Geoffrey-Tshipama <113897228+Geoffrey-Tshipama@users.noreply.github.com>
2024-08-13 13:42:05 +0100

    Update Test.py
    Nous affichons la multiplication de 2 matrice produit précédemment

--- Test.py
+++ Test.py
@@ -4,8 +4,7 @@ a = int(input("le nombre de colonne : ", ))
 b = int(input("le nombre de lignes : ", ))
 
 
-
-
 matrix = np.random.randn(a, b)
+double_matrix = np.dot(matrix, matrix)
 
-print(matrix)
+print(double_matrix)

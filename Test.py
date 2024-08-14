bb76b354e9cd9848a58943f53a6f41c9ed415828
Geoffrey Tshipama <geoffreytship3@gmail.com>
2024-08-14 18:58:12 +0100

    Ajout de la fonction choose qui fait un choix entre soustraction et addition, il retourne un message d'erreur dans le cas d'une mauvaise donnée en entrée

--- Test.py
+++ Test.py
@@ -2,27 +2,36 @@ import numpy as np
 
 a = int(input("le nombre de lignes matrice 1 : ", ))
 b = int(input("le nombre de lignes matrice 2 : ", ))
-c = int(input("Que souhaitez-vous au cas ou la multiplication échoue : ", ))
+
 
 CONSTANTE = 3
 
 matrix_1 = np.random.rand(a, CONSTANTE)
 matrix_2 = np.random.rand(b, CONSTANTE)
 
+
+def choose(c):
+    match c:
+        case 1:
+            somme = matrix_1 + matrix_2
+            print("La somme de matrice est ")
+            return somme
+        case 0:
+            difference = matrix_1 - matrix_2
+            print("La différence de matrice est ")
+            return difference
+        case _:
+            print("Donner 0 ou 1 entrée")
+
+
 if CONSTANTE == b:
     double_matrix = np.dot(matrix_1, matrix_2)
     print("la produit de matrice est ")
     print(double_matrix)
 else:
+    print("La multiplication à échoué")
+    c = int(input("Que souhaitez-vous faire la soustraction(0) ou l'addition(1) ? : ", ))
     try:
-        match c:
-            case 1:
-                somme = matrix_1 + matrix_2
-                print("La somme de matrice est ")
-                print(somme)
-            case 0:
-                difference = matrix_1 - matrix_2
-                print("La différence de matrice est ")
-                print(difference)
+        choose(c)
     except:
-        print("Le matrice sont incompatible pour la faire une sommation")
+        print("Les matrices sont incompatible pour la faire une sommation")

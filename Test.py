d86cd503ba6b0ffcd8cf110f476601c2491b5e91
Geoffrey Tshipama <geoffreytship3@gmail.com>
2024-08-14 16:20:12 +0100

    Je viens d'ajouter l'option de faire le produit ou la somme de 2 matrice en fonction du size des matrices. En cas d'incomptabilité, le code affiche un message

--- Test.py
+++ Test.py
@@ -1,10 +1,21 @@
 import numpy as np
 
-a = int(input("le nombre de colonne : ", ))
-b = int(input("le nombre de lignes : ", ))
+a = int(input("le nombre de lignes matrice 1 : ", ))
+b = int(input("le nombre de lignes matrice 2 : ", ))
 
+CONSTANTE = 3
 
-matrix = np.random.randn(a, b)
-double_matrix = np.dot(matrix, matrix)
+matrix_1 = np.random.rand(a, CONSTANTE)
+matrix_2 = np.random.rand(b, CONSTANTE)
 
-print(double_matrix)
+if CONSTANTE == b:
+    double_matrix = np.dot(matrix_1, matrix_2)
+    print("la produit de matrice est ")
+    print(double_matrix)
+else:
+    try:
+        somme = matrix_1 + matrix_2
+        print("La somme de matrice est ")
+        print(somme)
+    except:
+        print("Le matrice sont incompatible pour la faire une sommation")
